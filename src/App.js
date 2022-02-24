465ec66950773c997d1af496e961c95731bd63ec
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2022-02-24 12:13:59 +0100

    feat: Import child components and store provider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import PhoneDetailComponent from './components/PhoneDetailComponent';
+import PhoneListContainer from './components/PhoneListContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Header />
+    <Routes>
+      <Route exact path="/phones" element={<PhoneListContainer />} />
+      <Route path="/phone/:id" element={<PhoneDetailComponent />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
