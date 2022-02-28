689b61c16405a0139d1ad8a88c102c749783d387
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2022-02-28 12:49:18 +0100

    feat: Update detail component

--- src/components/PhoneDetailComponent.js
+++ src/components/PhoneDetailComponent.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import PhoneListContainer from './PhoneListContainer';
+import { NavLink, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const PhoneDetailComponent = () => (
-  <PhoneListContainer />
-);
+const PhoneDetailComponent = () => {
+  const phones = useSelector((state) => state.PhoneData.phones);
+  const { id } = useParams();
+  const phone = phones.find((phone) => phone.id == id);
+  const {
+    description, title, price, color, image,
+  } = phone;
+  return (
+    <div className="detailsContainer">
+      <div>
+        <NavLink to="/phones" className="back">
+          <p>Go Back</p>
+        </NavLink>
+      </div>
+      <h2 className="title">{title}</h2>
+      <div className="imgContainer">
+        <img className="img" src={image} alt={title} />
+      </div>
+      <p className="description">{description}</p>
+      <div className="flex">
+        <p className="description">{color}</p>
+        <p className="description">
+          $
+          {price}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default PhoneDetailComponent;
